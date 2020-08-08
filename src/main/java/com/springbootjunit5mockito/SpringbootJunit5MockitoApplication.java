a0755bc699e5e86098f05485c1e699db8576696d
mesut zincir <mesutzincir@gmail.com>
2020-08-08 21:00:26 +0100

    initial commit

---
+++ src/main/java/com/springbootjunit5mockito/SpringbootJunit5MockitoApplication.java
@@ -0,0 +1,13 @@
+package com.springbootjunit5mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJunit5MockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJunit5MockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootjunit5mockito/SpringbootJunit5MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootjunit5mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJunit5MockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
