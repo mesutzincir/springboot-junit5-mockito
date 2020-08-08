33506ee465e4f7758da25c52255ee86cf9d22da3
mesut zincir <mesutzincir@gmail.com>
2020-08-09 00:02:12 +0100

    unused imports were removed

--- src/test/java/com/springbootjunit5mockito/controllers/CustomerControllerTest.java
+++ src/test/java/com/springbootjunit5mockito/controllers/CustomerControllerTest.java
@@ -13,8 +13,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 
 @ExtendWith(MockitoExtension.class) //activate Mockito
 class CustomerControllerTest {
--- src/test/java/com/springbootjunit5mockito/services/CustomerServiceIntegrationTest.java
+++ src/test/java/com/springbootjunit5mockito/services/CustomerServiceIntegrationTest.java
@@ -1,19 +1,13 @@
 package com.springbootjunit5mockito.services;
 
 import com.springbootjunit5mockito.models.Customer;
-import com.springbootjunit5mockito.repositories.ICustomerRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Arrays;
 import java.util.List;
 
 @ExtendWith(SpringExtension.class)
